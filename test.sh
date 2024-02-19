a9f94ec2a5e34c259f45cd43b8b62c101149d90b
Laura Praca Lacerda <lpraca-l@f0r1s10.codam.nl>
2024-02-19 13:28:15 +0100

    loops both folders and valgrind

--- test.sh
+++ test.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+# Function that Loops through each file
+function loops() {
+  # Check if the maps folder exists
+  if [ ! -d "$maps_folder" ]; then
+      echo "Maps folder not found."
+      exit 1
+  fi
+  for map_file in "$maps_folder"/*; do
+      # Check if the file is a regular file
+      if [ -f "$map_file" ]; then
+          echo "$map_file"
+          # Run the program with the map file as an argument
+          valgrind ./cub3d "$map_file" &
+          # Store the process ID of the program
+          pid=$!
+          # Wait for a moment to ensure the program starts
+          sleep 2
+           # Check if the process exists before trying to kill it
+          if kill -0 "$pid" > /dev/null 2>&1; then
+            # Send signal to close the program
+            kill -SIGTERM "$pid"
+            # Wait for the program to close
+            wait "$pid"
+          fi
+          # Wait for the program to close
+          sleep 1
+          # Add a couple of new lines
+          echo -e "\n\n"
+      fi
+  done
+}
+
 # Path to the maps folder
 maps_folder="maps/invalid_maps"
-
-# Check if the maps folder exists
-if [ ! -d "$maps_folder" ]; then
-    echo "Maps folder not found."
-    exit 1
-fi
-
-# Loop through each file in the maps folder
-for map_file in "$maps_folder"/*; do
-    # Check if the file is a regular file
-    if [ -f "$map_file" ]; then
-        # Run the program with the map file as an argument
-        ./cub3d "$map_file" &
-        # Wait for a moment to ensure the program starts
-        sleep 2
-        # Send Escape key to close the program
-        xdotool key Escape
-        # Wait for the program to close
-        sleep 1
-    fi
-done
+loops
+maps_folder="maps/valid_maps"
+loops
